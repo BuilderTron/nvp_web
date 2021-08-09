208d1fe94a234fffc83913041b89c3adaffd1134
BuilderTron <jj.lopezbuilder@gmail.com>
2021-08-08 23:19:12 -0500

    Debug set to False.

--- nvp_web_2021/settings.py
+++ nvp_web_2021/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7j(o75-r#vmn+=)q1q*%i0gj%6p_r*_z!j=(5%$fb-e+x=%1wn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
